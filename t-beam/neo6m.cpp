ce47048e0b2dab1bcbf7b8ccc1aaf6a8e98789df
joepschyns <joepschyns@gmail.com>
2018-11-25 16:57:44 +0100

    test ack

--- t-beam/neo6m.cpp
+++ t-beam/neo6m.cpp
@@ -10,12 +10,75 @@ Neo6m::Neo6m() {
   neoSerial.begin(9600, SERIAL_8N1, GPS_TX, GPS_RX);
 }
 
-size_t Neo6m::turnOff() { //TODO implement  UBX-ACK
+void Neo6m::turnOff() { //TODO implement  UBX-ACK
   Neo6m::softwareReset(); //sleep_mode can only be activated at start up?
   delay(300); //give some time to restart //TODO wait for ack
-  return neoSerial.write(RXM_PMREQ, sizeof(RXM_PMREQ));
+  neoSerial.write(RXM_PMREQ, sizeof(RXM_PMREQ));
+  getUBX_ACK(RXM_PMREQ);
 }
 
-size_t Neo6m::softwareReset() {
-  return neoSerial.write(CFG_RST, sizeof(CFG_RST));
+void Neo6m::softwareReset() {
+  neoSerial.write(CFG_RST, sizeof(CFG_RST));
+  getUBX_ACK(CFG_RST);
+}
+
+//read ack based on https://ukhas.org.uk/guides:ublox6
+bool Neo6m::getUBX_ACK(const uint8_t *MSG) {
+  uint8_t b;
+  uint8_t ackByteID = 0;
+  uint8_t ackPacket[10];
+  unsigned long startTime = millis();
+  Serial.print(" * Reading ACK response: ");
+ 
+  // Construct the expected ACK packet    
+  ackPacket[0] = 0xB5;  // header
+  ackPacket[1] = 0x62;  // header
+  ackPacket[2] = 0x05;  // class
+  ackPacket[3] = 0x01;  // id
+  ackPacket[4] = 0x02;  // length
+  ackPacket[5] = 0x00;
+  ackPacket[6] = MSG[2];  // ACK class
+  ackPacket[7] = MSG[3];  // ACK id
+  ackPacket[8] = 0;   // CK_A
+  ackPacket[9] = 0;   // CK_B
+ 
+  // Calculate the checksums
+  for (uint8_t i=2; i<8; i++) {
+    ackPacket[8] = ackPacket[8] + ackPacket[i];
+    ackPacket[9] = ackPacket[9] + ackPacket[8];
+  }
+ 
+  while (1) {
+ 
+    // Test for success
+    if (ackByteID > 9) {
+      // All packets in order!
+      Serial.println(" (SUCCESS!)");
+      return true;
+    }
+ 
+    // Timeout if no valid response in 3 seconds
+    if (millis() - startTime > 6000) { 
+      Serial.println(" (FAILED!)");
+      return false;
+    }
+ 
+    // Make sure data is available to read
+    if (neoSerial.available()) {
+      Serial.println("serial online");
+      b = neoSerial.read();
+      Serial.print(b, HEX);
+       Serial.print(" == ");
+      Serial.println(ackPacket[ackByteID], HEX);
+      // Check that bytes arrive in sequence as per expected ACK packet
+      if (b == ackPacket[ackByteID]) { 
+        ackByteID++;
+        Serial.print(b, HEX);
+      } 
+      else {
+        ackByteID = 0;  // Reset and look again, invalid order
+      }
+ 
+    }
+  }
 }
--- t-beam/neo6m.h
+++ t-beam/neo6m.h
@@ -11,9 +11,11 @@ class Neo6m
 {
   public:
     Neo6m();
-    size_t turnOff();
-    size_t softwareReset();
+    void turnOff();
+    void softwareReset();
+    
   private:
+  bool getUBX_ACK(const uint8_t *MSG);
   //u-blox 6 Receiver Description 9.5 Power On/Off command and 36.3 RXM-PMREQ
   const byte RXM_PMREQ[16] = {0xb5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4d, 0x3b};   //power off until wakeup call
   //31.19 CFG-RST (0x06 0x04) checksum calculation https://gist.github.com/tomazas/3ab51f91cdc418f5704d
